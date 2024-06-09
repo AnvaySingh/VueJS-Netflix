a51ad9a25b30e2ae546d0e58b8df7ff3941d7731
AnvaySingh <anvaysingh05@gmail.com>
2024-06-09 20:29:38 +0530

    Home page changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './assets/main.css' //Import the main.css path that was created
 import './stores/movie.js'
+import './components/MovieDetails.vue'
 import App from './App.vue'
 
 const pinia = createPinia()
